921ac87724085f45ef1487136e2b019b116bc787
jvaraujos <39956177+jvaraujos@users.noreply.github.com>
2022-02-01 11:25:01 -0300

    including errors

--- formik-example/src/App.js
+++ formik-example/src/App.js
@@ -1,6 +1,6 @@
-import './App.css';
+import './App.css'
 import React from 'react'
-import { Formik, Field, Form } from 'formik';
+import { Formik, Field, Form } from 'formik'
 
 
 function App() {
@@ -8,23 +8,36 @@ function App() {
     console.log('SUBMIT', values)
   }
 
+  function validate(values) {
+    const errors = {}
+    if (!values.name) {
+      errors.name = 'Nome e obrigatorio'
+    }
+    if (!values.email) {
+      errors.email = "Email e obrigatorio"
+    }
+    return errors
+  }
   return (
     <div className="App">
       <Formik
+        validate={validate}
         onSubmit={onSubmit}
         initialValues={{
           name: '',
           email: ''
         }}
-        render={({ values }) => (
+        render={({ values, errors }) => (
           <Form >
             <div>
               <label>Nome</label>
               <Field name="name" type="text" />
+              {errors.name && (<span>{errors.name}</span>)}
             </div>
             <div>
               <label>Email</label>
               <Field name="email" type="text" />
+              {errors.email && (<span>{errors.email}</span>)}
             </div>
             <button type="submit">Enviar</button>
           </Form>
@@ -33,7 +46,7 @@ function App() {
       </Formik>
 
     </div>
-  );
+  )
 }
 
 export default App;
