c457648e4424d78059871f0b873d11bb2855e49f
jvaraujos <39956177+jvaraujos@users.noreply.github.com>
2022-02-01 11:17:41 -0300

    including submit

--- formik-example/src/App.js
+++ formik-example/src/App.js
@@ -4,23 +4,29 @@ import { Formik, Field } from 'formik';
 
 
 function App() {
+  function onSubmit(values, actions) {
+    console.log('SUBMIT', values)
+  }
+
   return (
     <div className="App">
       <Formik
+        onSubmit={onSubmit}
         initialValues={{
           name: '',
           email: ''
         }}
-        render={({ values, handleChange }) => (
-          <form>
+        render={({ values, handleSubmit }) => (
+          <form onSubmit={handleSubmit}>
             <div>
               <label>Nome</label>
-              <Field name="name" type="text" value={values.name} />
+              <Field name="name" type="text" />
             </div>
             <div>
               <label>Email</label>
-              <Field name="email" type="text" value={values.email} />
+              <Field name="email" type="text" />
             </div>
+            <button type="submit">Enviar</button>
           </form>
         )}>
 
