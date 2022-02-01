1b39d1916935218aeb2f009f0847dbcb32a21ad0
jvaraujos <39956177+jvaraujos@users.noreply.github.com>
2022-02-01 11:13:42 -0300

    including field

--- formik-example/src/App.js
+++ formik-example/src/App.js
@@ -1,21 +1,9 @@
 import './App.css';
 import React, { useState } from 'react'
-import { Formik } from 'formik';
+import { Formik, Field } from 'formik';
 
 
 function App() {
-
-  const [values, setValues] = useState({
-    name: '',
-  })
-
-  function onChange(ev) {
-    setValues({
-      ...values,
-      [ev.target.name]: ev.target.value,
-    })
-  }
-
   return (
     <div className="App">
       <Formik
@@ -27,11 +15,11 @@ function App() {
           <form>
             <div>
               <label>Nome</label>
-              <input name="name" type="text" value={values.name} onChange={handleChange} />
+              <Field name="name" type="text" value={values.name} />
             </div>
             <div>
               <label>Email</label>
-              <input name="email" type="text" value={values.email} onChange={handleChange} />
+              <Field name="email" type="text" value={values.email} />
             </div>
           </form>
         )}>
